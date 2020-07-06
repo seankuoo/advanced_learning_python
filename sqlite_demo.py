c59a112725af584323f06fcf2c83d44d77cc37e9
candidpinenut <seankuoo@gmail.com>
2020-07-06 16:46:02 +0800

    SQLite

--- sqlite_demo.py
+++ sqlite_demo.py
@@ -5,27 +5,58 @@ conn = sqlite3.connect("employee.db")
 
 c = conn.cursor()
 
-# c.execute(
-#     """CREATE TABLE employees(
-#     first text,
-#     last text,
-#     pay integer
-#     )""")
+c.execute(
+    """CREATE TABLE employees(
+    first text,
+    last text,
+    pay integer
+    )"""
+)
+
+
+def insert_emp(emp):
+    with conn:
+        c.execute(
+            "INSERT INTO employees VALUES(:first, :last, :pay)",
+            {"first": emp.first, "last": emp.last, "pay": emp.pay},
+        )
+
+
+def get_emps_by_name(lastname):
+    c.execute("SELECT * FROM employees WHERE last=:last", {"last": lastname})
+    return c.fetchall()
+
+
+def update_pay(emp, pay):
+    with conn:
+        c.execute(
+            """UPDATE employees SET pay = :pay WHERE first = :first AND last = :last""",
+            {"first": emp.first, "last": emp.last, "pay": pay},
+        )
+
+
+def remove_emp(emp):
+    with conn:
+        c.execute(
+            "DELETE from employees WHERE first = :first AND last = :last",
+            {"first": emp.first, "last": emp.last},
+        )
+
 
 emp_1 = Employee("John", "Doe", 80000)
 emp_2 = Employee("Jane", "Doe", 90000)
 
-print(emp_1.first)
-print(emp_1.last)
-print(emp_1.pay)
+insert_emp(emp_1)
+insert_emp(emp_2)
 
-# c.execute("INSERT INTO employees VALUES('Mary', 'Schafer', 70000)")
+emps = get_emps_by_name("Doe")
+print(emps)
 
-# conn.commit()
+update_pay(emp_2, 95000)
+remove_emp(emp_1)
 
-c.execute("SELECT * FROM employees WHERE last='Schafer'")
-
-print(c.fetchall())
+emps = get_emps_by_name("Doe")
+print(emps)
 
 conn.commit()
 
