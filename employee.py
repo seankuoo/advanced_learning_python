0ef3378647fcc6da7c694f276ad6160d0475033e
candidpinenut <seankuoo@gmail.com>
2020-07-07 13:26:53 +0800

    Logging_basic

--- employee.py
+++ employee.py
@@ -1,10 +1,24 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(levelname)s:%(message)s")
+
+file_handler = logging.FileHandler("employee.log")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+
 class Employee:
     """A sample Employee class"""
 
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         self.first = first
         self.last = last
-        self.pay = pay
+
+        logger.info("Created Employee: {} - {}".format(self.fullname, self.email))
 
     @property
     def email(self):
@@ -14,5 +28,10 @@ class Employee:
     def fullname(self):
         return "{} {}".format(self.first, self.last)
 
-    def __repr__(self):
-        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+    # def __repr__(self):
+    #     return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+
+emp_1 = Employee("John", "Smith")
+emp_2 = Employee("Corey", "Schafer")
+emp_3 = Employee("Jane", "Doe")
